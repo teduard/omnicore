f3353cff288814e4f00c0a226539dd86add5533b
eduard <eduard.tutescu@gmail.com>
2026-02-19 20:51:56 +0200

    update routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 //import { BrowserRouter, Routes, Route, 
-import {RouterProvider,createBrowserRouter  } from "react-router-dom";
+import {BrowserRouter, RouterProvider,createBrowserRouter  } from "react-router-dom";
 import './index.css'
 // import App from './App.tsx'
 // import ExpenseDashboard from './apps/ExpenseApp/ExpenseDashboard.tsx';
@@ -16,11 +16,15 @@ import './index.css'
 
 import {AppRoutes} from './routes/index.tsx';
 
-const router = createBrowserRouter(AppRoutes);
+const router = createBrowserRouter(AppRoutes,
+  {
+    basename: import.meta.env.BASE_URL
+  }
+);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+      <RouterProvider router={router} />
     {/* <BrowserRouter>
       <Routes>
         <Route path="/sql" element={<SQL />} />
