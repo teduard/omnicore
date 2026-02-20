75bbbafc21b0f0a5ca69af05eea9e563c94580a1
eduard <eduard.tutescu@gmail.com>
2026-02-20 06:26:37 +0200

    fix loading time

--- src/ai/embedding.ts
+++ src/ai/embedding.ts
@@ -1,11 +1,19 @@
-import { pipeline } from "@xenova/transformers";
+import { pipeline, env } from "@xenova/transformers";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 let embedder: any;
 
+env.allowLocalModels = false;
+env.useBrowserCache = true;
+
+//env.allowRemoteModels = false;
+
 await initEmbedder();
 
 export async function initEmbedder() {
+//  env.allowLocalModels = true;
+//env.useBrowserCache = true;
+
   embedder = await pipeline(
     "feature-extraction",
     "Xenova/all-MiniLM-L6-v2"
@@ -20,5 +28,3 @@ export async function embed(text: string): Promise<Float32Array> {
 
   return output.data;
 }
-
-
